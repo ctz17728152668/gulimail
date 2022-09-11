ab4c956409c833ebe5b0392e4aee6a757fdb450d
c1z <1660598843@qq.com>
2022-09-11 22:38:26 +0800

    项目初始结构创建

---
+++ gulimail-ware/src/test/java/com/ctz/gulimail/ware/GulimailWareApplicationTests.java
@@ -0,0 +1,14 @@
+package com.ctz.gulimail.ware;
+
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimailWareApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
